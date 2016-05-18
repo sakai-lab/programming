d209df00b4cd6d5714354bf9da6b0bd4cb06780f
linalool <small40511@yahoo.co.jp>
2016-05-18 13:57:59 +0900

    q3

--- q2/rina/q3.cpp
+++ q2/rina/q3.cpp
@@ -7,45 +7,27 @@
 */
 
 #include <iostream>
+#include <regex>
 #include <string>
-#include <vector>
 
 using namespace std;
 
-// 指定文字(char)delimiterで分割する関数
-void split(vector<string> &v, const string &input_string,
-	const char &delimiter) {
-	string::size_type index = input_string.find_first_of(delimiter);
-
-	if(index != string::npos) {
-		v.push_back(input_string.substr(0, index));
-		split(v, input_string.substr(index+1), delimiter);
-	} else {
-		v.push_back(input_string);
-	}
-}
 
 int main() {
-	string address;
-	vector<string> v;
-	vector<string> vl;
+	string input;
+	smatch match;
+	regex re("^(.@)+@[^(.@)]+.[^(.@)]+");
 
 	// 入力
-	cin >> address;
+	cin >> input;
 
-	split(v, address, '@');
-	if(v.size() != 2) {
-		cout << "'@'This is NOT an address." << endl;
-		return 0;
+
+
+	if(regex_match(input, match,re)) {
+		cout << "This is an address." << endl;
+	} else {
+		cout << "This is NOT an address." << endl;
 	}
-
-	split(vl, v[1], '.');
-	if(vl.size() != 2) {
-		cout << "'.'This is NOT an address." << endl;
-		return 0;
-	}
-
-	cout << "This is an address." << endl;
 
 	return 0;
 }
